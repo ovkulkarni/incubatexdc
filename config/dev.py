cbd68f26313404909b8002716973d1dad97958a6
evanyeyeye <evanshi2013@yahoo.com>
2016-08-06 17:06:33 -0400

    fix

--- config/dev.py
+++ config/dev.py
@@ -4,6 +4,7 @@ APP_NAME = "IncubateX DC Project"
 SECRET_KEY = "key"
 DB_PATH = "database.db"
 ENVIRONMENT = "dev"
+DISPLAY_DEBUG_INFO = "False"
 TEMPLATES_AUTO_RELOAD = True
 FB_APP_ID = os.getenv("FB_APP_ID", "")
 FB_APP_SECRET = os.getenv("FB_APP_SECRET", "")
