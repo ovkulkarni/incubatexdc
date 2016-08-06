c3cd16e9ddd69df11113042ad49c8653d3323615
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-08-06 13:56:49 -0400

    update config

--- config/dev.py
+++ config/dev.py
@@ -1,6 +1,10 @@
+import os
+
 APP_NAME = "IncubateX DC Project"
 SECRET_KEY = "key"
 DB_PATH = "database.db"
 DISPLAY_DEBUG_INFO = False
 ENVIRONMENT = "dev"
 TEMPLATES_AUTO_RELOAD = True
+FB_APP_ID = os.getenv("FB_APP_ID", "")
+FB_APP_SECRET = os.getenv("FB_APP_SECRET", "")
