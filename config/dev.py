ec37e1ba13244a5509a390cd6ff58dda56ae5998
evan <evan@localhost.localdomain>
2016-08-06 13:58:16 -0400

    version

--- config/dev.py
+++ config/dev.py
@@ -1,6 +1,6 @@
 APP_NAME = "IncubateX DC Project"
 SECRET_KEY = "key"
 DB_PATH = "database.db"
-DISPLAY_DEBUG_INFO = False
+DISPLAY_DEBUG_INFO = True
 ENVIRONMENT = "dev"
 TEMPLATES_AUTO_RELOAD = True
