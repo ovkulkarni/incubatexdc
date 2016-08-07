6cb3a6375dd9945600bd0da623101029cebf22d9
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-08-07 11:16:10 -0400

    config

--- config/dev.py
+++ config/dev.py
@@ -3,7 +3,7 @@ import os
 APP_NAME = "Web Makeover"
 SECRET_KEY = "key"
 ENVIRONMENT = "dev"
-DISPLAY_DEBUG_INFO = "False"
+DISPLAY_DEBUG_INFO = False
 TEMPLATES_AUTO_RELOAD = True
 FB_APP_ID = os.getenv("FB_APP_ID", "")
 FB_APP_SECRET = os.getenv("FB_APP_SECRET", "")
